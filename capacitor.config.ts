bea98cc4d3ab2c482cf1a792f31720413d019e19
EduardoP103 <eduardopastor103@gmail.com>
2023-07-07 22:54:48 -0500

    Se empieza a crear el proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RapidWork',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
